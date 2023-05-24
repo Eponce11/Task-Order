7e2584bf87ebf80cf0b5ae6702e50d8d9630354b
Eric <ponceeric458@gmail.com>
2023-05-23 21:37:44 -0700

    update navbar

--- Client/src/components/Navbar/index.tsx
+++ Client/src/components/Navbar/index.tsx
@@ -3,7 +3,7 @@ const Navbar = () => {
 
 
     return (
-        <div className="w-full bg-primary text-white p-2 flex items-center">
+        <div className="w-full bg-primary text-white p-2 flex items-center mb-3">
             <h3 className="text-2xl mr-7">Task-Order</h3>
             <ul className="flex items-center">
                 <li className="mr-4">Workspaces</li>
