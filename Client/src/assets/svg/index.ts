2df1fe3ff3bb1446f590a905502b49a124be8214
Eric <ponceeric458@gmail.com>
2023-05-29 17:36:11 -0700

    svg assets

---
+++ Client/src/assets/svg/index.ts
@@ -0,0 +1,16 @@
+
+import ChevronDown from "./chevron-down.svg"
+import Board from "./board.svg"
+import Home from "./home.svg"
+import Template from "./template.svg"
+import Add from "./add.svg"
+import Settings from "./settings.svg"
+
+export {
+    ChevronDown,
+    Board,
+    Home,
+    Template,
+    Add,
+    Settings
+}
