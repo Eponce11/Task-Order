a03bf178f959b9f93d04456c3799c331983e2578
Eric Ponce <ponceeric458@gmail.com>
2023-06-20 20:42:39 -0700

    sqlalchemy setup

---
+++ Server/flask_app/models/user_model.py
@@ -0,0 +1,7 @@
+from flask_app import db
+
+class User(db_Model):
+  id = db.Column(db.Integer, primary_key=True)
+  first_name = db.Column(db.String, nullable=False)
+  last_name = db.Column(db.String, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
