af2b800d90bfdac01678711dc9f2e201d6302881
Eric Ponce <ponceeric458@gmail.com>
2023-06-23 21:40:25 -0700

    server update

--- Server/flask_app/__init__.py
+++ Server/flask_app/__init__.py
@@ -3,12 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_app.config.env import DB_CONNECTION
 
 
-
-# create the extension
-db = SQLAlchemy()
-# create the app
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION
-# initialize the app with the extension
-db.init_app(app)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
--- Server/flask_app/models/user_model.py
+++ Server/flask_app/models/user_model.py
@@ -1,6 +1,6 @@
 from flask_app import db
 
-class User(db_Model):
+class User(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   first_name = db.Column(db.String, nullable=False)
   last_name = db.Column(db.String, nullable=False)
