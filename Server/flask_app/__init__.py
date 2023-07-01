2507646b65b3f710b268e6a6cc73b84fde131b10
Eric Ponce <ponceeric458@gmail.com>
2023-06-30 21:45:19 -0700

    update init file

--- Server/flask_app/__init__.py
+++ Server/flask_app/__init__.py
@@ -2,18 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_app.config.env import DB_CONNECTION
 
-
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  first_name = db.Column(db.String, nullable=False)
-  last_name = db.Column(db.String, nullable=False)
-  email = db.Column(db.String, unique=True, nullable=False)
+from flask_app.models.user_model import User
+
 
 with app.app_context():
   db.create_all()
+
+
+
+
