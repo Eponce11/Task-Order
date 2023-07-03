41277530bf4f4349f30dff3f3593385bb4ee2332
Eric Ponce <ponceeric458@gmail.com>
2023-07-02 18:27:28 -0700

    user create validation

--- Server/flask_app/models/user_model.py
+++ Server/flask_app/models/user_model.py
@@ -1,4 +1,7 @@
 from flask_app import db
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 class User(db.Model):
   __tablename__ = "users"
@@ -7,3 +10,28 @@ class User(db.Model):
   last_name = db.Column(db.String(40), nullable=False)
   email = db.Column(db.String(50), unique=True, nullable=False)
   password = db.Column(db.String(60), nullable=False)
+
+  @staticmethod
+  def create_user_validator(formData):
+    errors = {}
+    
+    if len(formData['first_name']) < 2:
+      errors['first_name'] = 'First Name must be at least 2 characters'
+    
+    if len(formData['last_name']) < 2:
+      errors['last_name'] = 'Last Name must be at least 2 characters'
+
+    if not EMAIL_REGEX.match(formData['email']):
+      errors['email'] = 'Invalid email address'
+    else:
+      potential_email = db.session.execute(db.select(User).filter_by(email=formData['email'])).scalar()
+      if potential_email:
+        errors['email'] = 'Email in use'
+      
+    if len(formData['password']) < 8:
+      errors['password'] = 'Password must be at least 8 characters'
+    
+    if not formData['password'] == formData['confirm_password']:
+      errors['confirm_password'] = 'Must match password'
+
+    return errors
