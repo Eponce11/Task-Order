65cd01f100822b2ab1a7655100dd0ef52921ab83
Eric Ponce <ponceeric458@gmail.com>
2023-07-03 21:34:24 -0700

    allowed CORS

--- Server/flask_app/__init__.py
+++ Server/flask_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_app.config.env import DB_CONNECTION, APP_SECRET
 from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
@@ -11,6 +12,8 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DB_CONNECTION
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+CORS(app, origins=["http://localhost:3000","http://127.0.0.1:3000"])
 
 from flask_app.models.user_model import User
 
